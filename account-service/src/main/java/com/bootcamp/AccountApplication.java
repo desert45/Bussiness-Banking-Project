ccb05cd1126e70d39a70b36bacaa967746d2861d
Antony Fernandez <62619196+desert45@users.noreply.github.com>
2022-07-21 00:51:19 -0500

    commit afc

--- account-service/src/main/java/com/bootcamp/AccountApplication.java
+++ account-service/src/main/java/com/bootcamp/AccountApplication.java
@@ -1,7 +1,11 @@
 package com.bootcamp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AccountApplication {
@@ -10,4 +14,17 @@ public class AccountApplication {
 		SpringApplication.run(AccountApplication.class, args);
 	}
 
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+    @Value("${service.name}")
+    private String name;
+
+    @RequestMapping("/name")
+    String getMessage() {
+        return this.name;
+    }
 }
